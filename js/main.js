8e4186c1298c71f7b081116b5ddce7d931cba15a
Mohit <mohitrajputms123@gmail.com>
2022-06-13 22:41:36 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ var div_sizes=[];
 var divs=[];
 var margin_size;
 var cont=document.getElementById("array_container");
-cont.style="flex-direction:row";
+cont.style= "flex-direction: row";
 
 inp_gen.addEventListener("click",generate_array);
 inp_as.addEventListener("input",update_arraysize);
@@ -19,12 +19,13 @@ inp_as.addEventListener("input",update_arraysize);
 function generate_array()
 {
     cont.innerHTML="";
+    console.log("hii");
     for(var i=0;i<array_size;i++){
-        div_sizes[i]=Math.floor(Math.random()*0.5*(inp_as.ariaValueMax-inp_as.ariaValueMin));
-        div[i] = document.createElement("div");
-        cont.appendChild(div[i]);
+        div_sizes[i]=Math.floor(Math.random()*0.5*(inp_as.max-inp_as.min));
+        divs[i] = document.createElement("div");
+        cont.appendChild(divs[i]);
         margin_size=0.1;
-        div[i].style=" margin:0% "+margin_size+"%; background-color: blue; width:"+(100/array_size - 2*(margin_size))+"%; height:"+(div_sizes[i])+"%;";
+        divs[i].style=" margin:0% "+margin_size+"%; background-color: blueviolet; width:"+(100/array_size)+"%; height:"+(div_sizes[i])+"%;";
     }
 }
 
